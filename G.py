e943e3d72f7c95fbae3c99cbb00d7325fa9d4150
Arnav Angarkar <arnav.angarkar20@gmail.com>
2024-09-12 22:14:26 +0530

    Update G.py

--- G.py
+++ G.py
@@ -1,1 +1,12 @@
+def lel(n):
+    for i in range(n, 0, -1):
+        print(' ' * (n - i), end='')
+        print('*' * (2 * i - 1))
+   
+    for i in range(1, n + 1):
+        print(' ' * (n - i), end='')
+        print('*' * (2 * i - 1))
+
+n = int(input("Enter the number of rows for the pyramids: "))
+lel(n)
 
