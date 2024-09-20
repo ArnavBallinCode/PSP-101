a395192c0830dfa884822e08071fec9465bfc3e8
Arnav Angarkar <arnavangarkar@Arnavs-MacBook-Air.local>
2024-09-20 15:24:29 +0530

    lab2-question2

---
+++ lab2-question2.py
@@ -0,0 +1,6 @@
+phy_marks = float(input("Enter physics marks: "))
+chem_marks = float(input("Enter chemistry marks: "))
+math_marks = float(input("Enter math marks: "))
+avg_marks = (phy_marks + chem_marks + math_marks)/3
+print(avg_marks)
+
